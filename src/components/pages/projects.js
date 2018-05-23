b25fe0276d16e8605de0d10b5464764f51b4ebb4
Jessica Peters <jlpeters211@gmail.com>
2018-05-22 19:24:49 -0500

    Revert "created seperate card component"

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import ProjectCard from '../../components/ProjectCard';
-
 
 import {
   Link
@@ -9,25 +7,138 @@ import {
 class Projects extends Component {
   render() {
     return (
-      <div className="MainDiv">
 
       <div className = "Projects">
-        <div className="jumbotron">
+      <div className="jumbotron">
           <div className="container">
+
           </div>
       </div>
+      {/* BEGIN CARD SECTION */}
+      <main>
+  		<a href="#" title="Post 1">
+  			<article>
+  				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate itaque consequatur illo quis, delectus, incidunt labore.</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-1"/>
+  					<figcaption>Post 1</figcaption>
+  				</figure>
+  			</article>
+  		</a>
 
-      <div className="addProjectButton">
-        <h1>ADD NEW PROJECT FORM</h1>
-          <i className="material-icons">palette</i>
-        </div>
+  		<a href="#" title="Post 2">
+  			<article>
+  				<p>Tempore quas recusandae sint molestias illo laudantium temporibus possimus autem voluptatibus nemo, ratione impedit, amet eos!</p>
+  				<figure>
+  					<img src="https://placehold.it/480x340" alt="image-2"/>
+  					<figcaption>Post 2</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 3">
+  			<article>
+  				<p>Dolor necessitatibus at pariatur, beatae officiis non, consectetur quae recusandae sunt impedit alias voluptate voluptas amet.</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-3"/>
+  					<figcaption>Post 3</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 4">
+  			<article>
+  				<p>Voluptates voluptatibus veniam veritatis quia sequi ipsa itaque autem fugiat saepe sed, dignissimos, iusto cupiditate omnis.</p>
+  				<figure>
+  					<img src="https://placehold.it/480x320" alt="image-4"/>
+  					<figcaption>Post 4</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 5">
+  			<article>
+  				<p>Illo vitae temporibus ut nostrum ducimus maxime, fugit asperiores fugiat, optio quidem, repellat distinctio non saepe.</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-5"/>
+  					<figcaption>Post 5</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 6">
+  			<article>
+  				<p>Earum, modi nisi, quia, ipsam non commodi, minus mollitia amet explicabo molestiae fuga labore illum excepturi!</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-6"/>
+  					<figcaption>Post 6</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 7">
+  			<article>
+  				<p>Dolorum vel tempora, cum fugit, repellat quisquam distinctio quibusdam, aliquam sunt alias fugiat error, numquam saepe.</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-7"/>
+  					<figcaption>Post 7</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 8">
+  			<article>
+  				<p>Labore recusandae, odio fugit. Ipsa ipsam veritatis et autem ab nesciunt voluptatibus, ducimus in natus accusamus!</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-8"/>
+  					<figcaption>Post 8</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 9">
+  			<article>
+  				<p>Quis dolorum, repellat quibusdam, at debitis asperiores mollitia sunt placeat iure eveniet fugiat voluptatum nesciunt sed.</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-9"/>
+  					<figcaption>Post 9</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 10">
+  			<article>
+  				<p>Accusantium labore, quibusdam deleniti aliquam ducimus saepe nulla ipsa quisquam expedita distinctio tempora. Consectetur, repellendus, accusantium.</p>
+  				<figure>
+  					<img src="https://placehold.it/480x360" alt="image-10"/>
+  					<figcaption>Post 10</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 11">
+  			<article>
+  				<p>Aut vero assumenda architecto, autem id atque fuga placeat corporis, iure officia eligendi earum blanditiis nihil!</p>
+  				<figure>
+  					<img src="https://placehold.it/480x460" alt="image-11"/>
+  					<figcaption>Post 11</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 12">
+  			<article>
+  				<p>Commodi sint expedita, deserunt ipsum vitae tempore adipisci tenetur debitis architecto, repellat, quia cum error cupiditate!</p>
+  				<figure>
+  					<img src="https://placehold.it/480x340" alt="image-12"/>
+  					<figcaption>Post 12</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  		<a href="#" title="Post 13">
+  			<article>
+  				<p>Culpa nihil aspernatur dolor deserunt accusantium id facilis libero in magnam neque. At explicabo voluptas eius?</p>
+  				<figure>
+  					<img src="https://placehold.it/480x380" alt="image-13"/>
+  					<figcaption>Post 13</figcaption>
+  				</figure>
+  			</article>
+  		</a>
+  	</main>
+    {/* END CARD SECTION */}
       </div>
 
-      <div className="ProjectCard">
-      <ProjectCard />
-      </div>
 
-      </div>
     );
   }
 }
